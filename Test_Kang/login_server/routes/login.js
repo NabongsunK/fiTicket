9db44a9e10e018dd49ddb4219422662264a89026
kang <kangtu142@gmail.com>
2023-09-05 00:13:06 +0900

    로그인DB생성

--- Test_Kang/login_server/routes/login.js
+++ Test_Kang/login_server/routes/login.js
@@ -1,10 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+const LoginModel = require('../models/user.LoginModel');
+const pool = require('../models/pool');
 
 // 로그인 시도
-router.post('/getauthnum', async (req, res, next) => {
-  
+router.post('/signin', async (req, res, next) => {
+  try{
+    const result= await LoginModel.signIn(req.body);
+    res.json(result);
+  }catch(err){
+    next(err);
+  }
+});
+
+// 로그아웃
+router.post('/signout', async (req, res, next) => {
+  try{
+    const result= await LoginModel.signOut(req.body);
+    res.json(result);
+  }catch(err){
+    next(err);
+  }
+});
+
+// 회원가입
+router.post('/signup', async (req, res, next) => {
+  try{
+    const result= await LoginModel.signUp(req.body);
+    res.json(result);
+  }catch(err){
+    next(err);
+  }
 });
 
 
