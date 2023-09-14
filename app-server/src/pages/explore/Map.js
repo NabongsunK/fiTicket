7f206c6b04159bb40df4b5233fa7f576771623b3
kang <kangtu142@gmail.com>
2023-09-14 09:40:00 +0900

    fix: map속도저하 해결

--- app-server/src/pages/explore/Map.js
+++ app-server/src/pages/explore/Map.js
@@ -34,6 +34,7 @@ const getItude = async function (query="서울") {
         Authorization: "KakaoAK 8e8301f6d873da44dfc2345e960bae20"
       }
     });
+    //제대로 뽑아왔으면
     if(res.data.documents[0]){
       return ([res.data.documents[0].x,res.data.documents[0].y]);
     }
@@ -49,18 +50,34 @@ const getItude = async function (query="서울") {
 
 
 const Map= function(){
-  const [mapItude,setMapItude] = useState([126.978652258309,37.566826004661])
+  //경도,위도,사이즈
+  const [mapItude,setMapItude] = useState([126.978652258309,37.566826004661,8])
+  //onchange되면
   const onChangeQuery = async function (e){
     if(e.target.value){
-      return setMapItude(await getItude(e.target.value))
+      return setMapItude([...await getItude(e.target.value),3])
     }
   }
 
+  //첫마운트 될때
+  useEffect(function(){
+    // HTML5의 geolocation으로 사용할 수 있는지 확인합니다 
+  if (navigator.geolocation) {
+    // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+    navigator.geolocation.getCurrentPosition(function(position) {
+        var lat = position.coords.latitude, // 위도
+            lon = position.coords.longitude; // 경도
+        setMapItude([lon,lat,8]);            
+      });
+  }
+  },[])
+  //MapItude 바뀔때
+  // 여기서 지도를 계속 생성안해도 되는 방법은 없을까?
   useEffect(function(){
     var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     mapOption = { 
         center: new kakao.maps.LatLng(mapItude[1],mapItude[0]), // 지도의 중심좌표
-        level: 8 // 지도의 확대 레벨
+        level: mapItude[2] // 지도의 확대 레벨
     };
     var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
@@ -77,56 +94,6 @@ const Map= function(){
         var resultDiv = document.getElementById('result'); 
         resultDiv.innerHTML = message;
     });
-
-    // HTML5의 geolocation으로 사용할 수 있는지 확인합니다 
-    if (navigator.geolocation) {
-        
-      // GeoLocation을 이용해서 접속 위치를 얻어옵니다
-      navigator.geolocation.getCurrentPosition(function(position) {
-          
-          var lat = position.coords.latitude, // 위도
-              lon = position.coords.longitude; // 경도
-          
-          var locPosition = new kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
-              message = '<div style="padding:5px;">여기에 계신가요?!</div>'; // 인포윈도우에 표시될 내용입니다
-          
-          // 마커와 인포윈도우를 표시합니다
-          displayMarker(locPosition, message);
-              
-        });
-      
-    } else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
-      
-      var locPosition = new kakao.maps.LatLng(33.450701, 126.570667),    
-          message = 'geolocation을 사용할수 없어요..'
-          
-      displayMarker(locPosition, message);
-    }
-
-    // 지도에 마커와 인포윈도우를 표시하는 함수입니다
-    function displayMarker(locPosition, message) {
-
-      // 마커를 생성합니다
-      var marker = new kakao.maps.Marker({  
-          map: map, 
-          position: locPosition
-      }); 
-      
-      var iwContent = message, // 인포윈도우에 표시할 내용
-          iwRemoveable = true;
-
-      // 인포윈도우를 생성합니다
-      var infowindow = new kakao.maps.InfoWindow({
-          content : iwContent,
-          removable : iwRemoveable
-      });
-      
-      // 인포윈도우를 마커위에 표시합니다 
-      infowindow.open(map, marker);
-      
-      // 지도 중심좌표를 접속위치로 변경합니다
-      map.setCenter(locPosition);      
-    }    
     
   },[mapItude])
   return(
@@ -142,15 +109,24 @@ const Map= function(){
       <div id="map" style ={{
         width:'100%',
         height:'550px'
-      }}>
+      }}/>
 
+      {/* 여기서 왜 새로고침할때마다 새로되는거지?? 이거 state쓰면안될듯 */}
+      <div className="btn-group" role="group" aria-label="Basic radio toggle button group">
+        <input type="radio" className="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked/>
+        <label className="btn btn-outline-primary" for="btnradio1">Radio 1</label>
+
+        <input type="radio" className="btn-check" name="btnradio" id="btnradio2" autocomplete="off"/>
+        <label className="btn btn-outline-primary" for="btnradio2">Radio 2</label>
+
+        <input type="radio" className="btn-check" name="btnradio" id="btnradio3" autocomplete="off"/>
+        <label className="btn btn-outline-primary" for="btnradio3">Radio 3</label>
       </div>
-      <div id="result"></div>
+
+      <div id="result"/>
     </>
     
   )
 }
 
 export default Map;
-
-
