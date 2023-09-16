7f17359c43766ab1b34c27cc29f1c4554423a0c9
kang <kangtu142@gmail.com>
2023-09-16 13:56:07 +0900

    .

---
+++ app-server/src/pages/explore/BodyTop.js
@@ -0,0 +1,21 @@
+const BodyTop = function () {
+  return (
+    <div className="amazing-deals">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6 offset-lg-3">
+            <div className="section-heading text-center">
+              <h2>지금 바로 축제를 즐기세요!</h2>
+              <p>
+                여러분 주위에 여러 행사들이 열려있습니다.
+                <br />
+                Loca!T와 함께 다양한 축제를 지금 바로 만끽하시길 바랍니다.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default BodyTop;
--- app-server/src/pages/explore/index.js
+++ app-server/src/pages/explore/index.js
@@ -7,6 +7,8 @@ import MapDiv from "./MapDiv";
 import { useState } from "react";
 
 import axios from "axios";
+import SecondHead from "./SecondHead";
+import BodyTop from "./BodyTop";
 
 // axios 기본 url 정의
 axios.defaults.baseURL = "http://localhost:4400/api";
@@ -26,47 +28,11 @@ const Explore = function () {
 
   return (
     <>
-      <div className="second-page-heading">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-12">
-              <h4>지도에서 축제를 탐색하세요</h4>
-              <h2>축제를 찾아 바로 참가하세요</h2>
-              <p>
-                수많은 축제들이 당신을 기다리고 있습니다.
-                <br />
-                주저말고 지금 바로 즐기세요
-              </p>
-              <div className="border-button">
-                <Link
-                  to="/login"
-                  style={{ color: "#fff", borderColor: "#fff" }}
-                >
-                  로그인
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      {/* 두번째 헤더 */}
+      <SecondHead />
 
       {/* 지도 위 소제목 */}
-      <div className="amazing-deals">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-6 offset-lg-3">
-              <div className="section-heading text-center">
-                <h2>지금 바로 축제를 즐기세요!</h2>
-                <p>
-                  여러분 주위에 여러 행사들이 열려있습니다.
-                  <br />
-                  Loca!T와 함께 다양한 축제를 지금 바로 만끽하시길 바랍니다.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <BodyTop />
 
       {/* 본문*/}
       <div className="reservation-form">
