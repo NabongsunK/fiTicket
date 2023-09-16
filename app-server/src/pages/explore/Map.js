ae35c96faf3f2afaf7c62dc8f22a2f0b2d2bbc66
kang <kangtu142@gmail.com>
2023-09-16 10:46:30 +0900

    ⚰️ eslintrc 수정

--- app-server/src/pages/explore/Map.js
+++ app-server/src/pages/explore/Map.js
@@ -4,8 +4,13 @@ import chicken from "../../data/chicken.json";
 const { kakao } = window;
 
 const Map = function (props) {
+  var infoWindows = [];
+
+  function closeInfoWindow() {
+    infoWindows.forEach((infowindow) => infowindow.close());
+  }
+
   //첫마운트 될때, props변경될때
-  console.log(props);
   useEffect(
     function () {
       var map = new kakao.maps.Map(document.getElementById("map"), {
@@ -30,18 +35,22 @@ const Map = function (props) {
         });
 
         // 마커를 클릭했을 때 마커 위에 표시할 인포윈도우를 생성합니다
-        var iwContent = '<div style="padding:5px;">Hello World!</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-          iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
+        var iwContent = `<div style="padding:5px;">${[
+            position.lat,
+            position.lng,
+          ]}</div>`, // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+          iwRemoveable = false; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
 
         // 인포윈도우를 생성합니다
         var infowindow = new kakao.maps.InfoWindow({
           content: iwContent,
           removable: iwRemoveable,
         });
-
+        infoWindows.push(infowindow);
         // 마커에 클릭이벤트를 등록합니다
         kakao.maps.event.addListener(marker, "click", function () {
           // 마커 위에 인포윈도우를 표시합니다
+          closeInfoWindow();
           infowindow.open(map, marker);
         });
 
