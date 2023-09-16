ea8998d19363b4a00d59f27a977b577077697e61
kang <kangtu142@gmail.com>
2023-09-16 11:02:25 +0900

    ✨ explore/getallmap 추가

---
+++ api-server/services/explore.service.js
@@ -0,0 +1,31 @@
+const ExploreGetModel = require("../models/explore.GetModel");
+const pool = require("../models/pool");
+require("date-utils");
+
+//로그인상태를 어떻게 구별하지?
+//authDB랑 연결을 어떻게? 왜 시키지?
+//로그인시도일때 비밀번호만 틀렸을떄 알려주기
+
+const ExploreService = {
+  async getListForMap() {
+    const conn = await pool.getConnection();
+    try {
+      // 트랜젝션 작업 시작
+      await conn.beginTransaction();
+
+      const data = await ExploreGetModel.getListMap();
+      // DB에 작업 반영
+      await conn.commit();
+      return { ...data, ok: true };
+    } catch (err) {
+      // DB 작업 취소
+      await conn.rollback();
+      throw new Error("Service Error", { cause: err });
+    } finally {
+      // 커넥션 반납
+      pool.releaseConnection(conn);
+    }
+  },
+};
+
+module.exports = ExploreService;
