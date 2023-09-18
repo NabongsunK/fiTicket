34583dc3d5165c554db310bedd3da71167e63f63
HWANG <poka95@naver.com>
2023-09-18 17:00:53 +0900

    https://github.com/NabongsunK/localT.git

--- app-server/src/pages/home/index.js
+++ app-server/src/pages/home/index.js
@@ -1,15 +1,5 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import GoToMap from "../../components/common/GoToMap";
-
-const sliderItems = [
-  { id: 1, imgSrc: "path/to/image1.jpg", description: "1번" },
-  { id: 2, imgSrc: "path/to/image2.jpg", description: " 2번임" },
-  { id: 3, imgSrc: "path/to/image3.jpg", description: "3333333ㅋㅋ 3" },
-  { id: 4, imgSrc: "path/to/image4.jpg", description: "4ㅋ4ㅋ4ㅋ4ㅋ123 4" },
-  { id: 5, imgSrc: "path/to/image5.jpg", description: "이55이이이5" },
-  { id: 6, imgSrc: "path/to/image6.jpg", description: "이육육ㅁ닝 6" },
-];
 
 function Home() {
   return (
@@ -313,93 +303,26 @@ function Home() {
             </div>
           </div>
 
-          {/* 슬라이더 시작  */}
-          <div className="row mt-5">
-            <div
-              id="carouselExampleControls"
-              className="carousel slide"
-              data-bs-ride="carousel"
-            >
-              <div className="carousel-inner">
-                {/* 첫번째 슬라이더 */}
-                <div className="carousel-item active">
-                  <div className="d-flex justify-content-center">
-                    {sliderItems.slice(0, 3).map((item) => (
-                      <div
-                        key={item.id}
-                        className="m-3 p-5"
-                        style={{
-                          border: "1px solid black",
-                          borderRadius: "20px",
-                          width: "200px",
-                          textAlign: "center",
-                        }}
-                      >
-                        <img
-                          src={item.imgSrc}
-                          alt={item.description}
-                          style={{ width: "80%", height: "auto" }}
-                        />
-                        <p>{item.description}</p>
-                      </div>
-                    ))}
-                  </div>
-                </div>
-
-                {/* 두번째 슬라이더 */}
-                <div className="carousel-item">
-                  <div className="d-flex justify-content-center">
-                    {sliderItems.slice(3, 6).map((item) => (
-                      <div
-                        key={item.id}
-                        className="m-3 p-5"
-                        style={{
-                          border: "1px solid black",
-                          borderRadius: "20px",
-                          width: "200px",
-                          textAlign: "center",
-                        }}
-                      >
-                        <img
-                          src={item.imgSrc}
-                          alt={item.description}
-                          style={{ width: "80%", height: "auto" }}
-                        />
-                        <p>{item.description}</p>
-                      </div>
-                    ))}
-                  </div>
-                </div>
+          <div className="row align-items-center">
+            <div className="col-1">
+              <div className="border-button">
+                <Link to="/">
+                  <i className="fa fa-angle-left fs-2"></i>
+                </Link>
               </div>
-
-              {/* 슬라이더 컨트롤*/}
-              <button
-                className="carousel-control-prev"
-                type="button"
-                data-bs-target="#carouselExampleControls"
-                data-bs-slide="prev"
-              >
-                <span
-                  className="carousel-control-prev-icon"
-                  aria-hidden="true"
-                ></span>
-                <span className="visually-hidden">Previous</span>
-              </button>
-              <button
-                className="carousel-control-next"
-                type="button"
-                data-bs-target="#carouselExampleControls"
-                data-bs-slide="next"
-              >
-                <span
-                  className="carousel-control-next-icon"
-                  aria-hidden="true"
-                ></span>
-                <span className="visually-hidden">Next</span>
-              </button>
+            </div>
+            <div className="col-10">
+              <img
+                src="assets/images/review_temp.png"
+                style={{ width: "120%" }}
+              />
+            </div>
+            <div className="col-1">
+              <div className="border-button">
+                <Link to="/"><i className="fa fa-angle-right fs-2"></i></Link>
+              </div>
             </div>
           </div>
-          {/* 슬라이더 끝 */}
         </div>
       </div>
       <GoToMap />
