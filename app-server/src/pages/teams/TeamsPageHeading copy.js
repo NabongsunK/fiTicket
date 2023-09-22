acedfc1879667b32da1f7c684cded2904c1e3a82
DESKTOP-0QVVBKM\USER <schoug@naver.com>
2023-09-22 22:03:50 +0900

    teams 페이지 연습

--- app-server/src/pages/teams/TeamsPageHeading copy.js
+++ app-server/src/pages/teams/TeamsPageHeading copy.js
@@ -1,8 +1,17 @@
 import { Link } from "react-router-dom";
 import OwlCarousel from "react-owl-carousel";
+import { useState } from "react";
 // import userPic from "../../../public/assets/images/best-02.jpg";
 
 const TeamsPageHeading2 = function () {
+  const [isActive, setActive] = useState("false");
+  const alertHandler = () => {
+    setActive(!isActive);
+    setTimeout(() => {
+      setActive(isActive);
+    }, 3000);
+  };
+
   //Owl Carousel Settings
   const options = {
     loop: true,
@@ -292,6 +301,53 @@ const TeamsPageHeading2 = function () {
                 }
               </OwlCarousel>
             </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="col-sm-6">
+        <div className="card">
+          <div className="card-header">
+            <h5>Data-delay</h5>
+          </div>
+          <div className="card-body">
+            <div className="bg-light p-4 mb-2" style={{ height: "250px" }}>
+              <div
+                className={
+                  isActive
+                    ? "toast toast-3s fade hide"
+                    : "toast toast-3s fade show"
+                }
+                role="alert"
+                aria-live="assertive"
+                data-delay="3000"
+                aria-atomic="true"
+              >
+                <div className="toast-header">
+                  <img
+                    src="assets/images/favicon.ico"
+                    alt=""
+                    className="img-fluid m-r-5"
+                    style={{ width: "20px" }}
+                  />
+                  <strong className="mr-auto">Loca!T와 축제를 즐기세요</strong>
+                  <small className="text-muted">완료</small>
+                  <button
+                    type="button"
+                    className="m-l-5 mb-1 mt-1 close"
+                    data-dismiss="toast"
+                    aria-label="Close"
+                  >
+                    <span>×</span>
+                  </button>
+                </div>
+                <div className="toast-body">티켓이 장바구니에 담겼습니다.</div>
+              </div>
+            </div>
+
+            <button className="btn  btn-primary" onClick={alertHandler}>
+              3 sec
+            </button>
           </div>
         </div>
       </div>
