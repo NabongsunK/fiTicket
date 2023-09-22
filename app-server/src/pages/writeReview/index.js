b323bcbc98ff10d2411cc48f7a996c8feab50f59
kang <kangtu142@gmail.com>
2023-09-22 15:34:49 +0900

    .

---
+++ app-server/src/pages/writeReview/index.js
@@ -0,0 +1,12 @@
+import CommentList from "./CommentList";
+import Payment from "./Payment";
+
+function Geuljae() {
+  return (
+    <div>
+      <CommentList />
+    </div>
+  );
+}
+
+export default Geuljae;
