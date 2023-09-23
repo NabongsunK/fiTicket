198539e4b4bd8b5e1ebc7a3e75af48b30fa22e6b
kang <kangtu142@gmail.com>
2023-09-23 15:45:37 +0900

    .

--- app-server/src/components/common/UserList.js
+++ app-server/src/components/common/UserList.js
@@ -1,7 +1,6 @@
 import UserListItem from "./UserListItem";
 
 function UserList(props) {
-  console.log(props.userItems);
   var userArr = props.userItems.map((item, index) => (
     <UserListItem key={index} item={item} handleToggle={props.handleToggle} />
   ));
