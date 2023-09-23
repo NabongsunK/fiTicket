2b95ca629b185310bac9afc52574fa00af701c93
DESKTOP-0QVVBKM\USER <schoug@naver.com>
2023-09-23 10:49:27 +0900

    deals 사진 채우기, 링크 조정, 카트 알림

--- app-server/src/pages/deals/DealsListItem.js
+++ app-server/src/pages/deals/DealsListItem.js
@@ -1,64 +1,91 @@
 import { Link } from "react-router-dom";
+import { push, pop } from "../../store/cartSlice";
 
+import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 // axios 기본 url 정의
 axios.defaults.baseURL = "http://localhost:4400/api";
 
 const DealsListItem = function (props) {
+  const dispatch = useDispatch();
+  const myCart = useSelector((state) => state.myCartSlice.myCarts);
+
+  const toCart = function () {
+    dispatch(
+      push({
+        ticket: {
+          // 여기 지역추가
+          badge: props.festival.addr1 + " " + props.festival.addr2,
+          name: props.festival.title,
+          quantity: 1,
+          // 여기 가격추가
+          price: props.festival.id,
+          image: props.festival.first_image2,
+          ticket_id: props.festival.id,
+          index: myCart.length,
+        },
+      })
+    );
+  };
+
+  const poster =
+    props.festival.first_image === "" ? (
+      <img src="assets/images/fes_default.jpg" alt="" />
+    ) : (
+      <img src={props.festival.first_image} alt="" />
+    );
   return (
     <div className="col-lg-6 col-sm-6">
-      <Link to={`/explore/${props.festival.id}`}>
-        <div className="item">
-          <div className="row">
-            <div className="col-lg-6">
-              <div className="image">
-                <img src={props.festival.first_image} alt="" />
-                {/* <img src="http://tong.visitkorea.or.kr/cms/resource/75/2769775_image2_1.JPG" alt="" /> */}
-              </div>
+      <div className="item">
+        <div className="row">
+          <div className="col-lg-6">
+            <div className="image">
+              <Link to={`/explore/${props.festival.id}`}>{poster}</Link>
+              {/* <img src="http://tong.visitkorea.or.kr/cms/resource/75/2769775_image2_1.JPG" alt="" /> */}
             </div>
-            <div className="col-lg-6 align-self-center">
-              <div className="content">
-                <span className="info">*D-{props.festival.d_day}</span>
+          </div>
+          <div className="col-lg-6 align-self-center">
+            <div className="content">
+              <span className="info">*D-{props.festival.d_day}</span>
+              <Link to={`/explore/${props.festival.id}`}>
                 <h4>{props.festival.title}</h4>
-                <div className="row">
-                  <div className="col-11">
-                    <i className="fa fa-clock"></i>
-                    <span className="list">
-                      {props.festival.event_start_date} -
-                      {props.festival.event_end_date}
-                    </span>
+              </Link>
+
+              <div className="row">
+                <div className="col-11">
+                  <i className="fa fa-clock"></i>
+                  <span className="list">
+                    {props.festival.event_start_date} -
+                    {props.festival.event_end_date}
+                  </span>
+                </div>
+              </div>
+              <p>
+                {props.festival.over_view.substring(0, 64)}{" "}
+                {props.festival.over_view.length > 64 ? "......" : ""}
+              </p>
+              <div className="row">
+                <div className="col-lg-3">
+                  {/* 홈페이지 연결 */}
+                  <div className="explore_list_button">
+                    <Link to="https://www.chf.or.kr/short/8sQs" target="_blank">
+                      <i className="fa fa-home"></i>
+                    </Link>
                   </div>
                 </div>
-                <p>
-                  {props.festival.over_view.substring(0, 64)}{" "}
-                  {props.festival.over_view.length > 64 ? "......" : ""}
-                </p>
-                <div className="row">
-                  <div className="col-lg-3">
-                    {/* 홈페이지 연결 */}
-                    <div className="explore_list_button">
-                      <Link
-                        to="https://www.chf.or.kr/short/8sQs"
-                        target="_blank"
-                      >
-                        <i className="fa fa-home"></i>
-                      </Link>
-                    </div>
-                  </div>
-                  <div className="col-lg-3">
-                    {/* 장바구니 담기 */}
-                    <div className="explore_list_button">
-                      <Link to="/">
-                        <i className="fa fa-cart-plus"></i>
-                      </Link>
-                    </div>
+                <div className="col-lg-3">
+                  {/* 장바구니 담기 */}
+                  <div className="explore_list_button" onClick={toCart}>
+                    <Link onClick={props.alertHandler}>
+                      <i className="fa fa-cart-plus"></i>
+                    </Link>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </Link>
+      </div>
     </div>
   );
 };
--- app-server/src/pages/deals/index.js
+++ app-server/src/pages/deals/index.js
@@ -20,6 +20,14 @@ const allRecommends = async function () {
 var recommendsList = await allRecommends();
 
 const Deal = function () {
+  const [isActive, setActive] = useState("false");
+  const alertHandler = () => {
+    setActive(!isActive);
+    setTimeout(() => {
+      setActive(isActive);
+    }, 3000);
+  };
+
   //페이징 처리
 
   const page = useSelector((state) => state.viewPageSlice.page);
@@ -41,7 +49,11 @@ const Deal = function () {
   const dispatch = useDispatch();
 
   const list = pageResult.map((festival) => (
-    <DealsListItem key={festival.id} festival={festival} />
+    <DealsListItem
+      key={festival.id}
+      festival={festival}
+      alertHandler={alertHandler}
+    />
   ));
 
   return (
@@ -50,6 +62,31 @@ const Deal = function () {
 
       {/* 필터 */}
       <Filter />
+      {/* 알람창 놓고싶은데 넣기*/}
+      <div
+        className={
+          isActive ? "toast toast-3s fade hide" : "toast toast-3s fade show"
+        }
+        role="alert"
+        aria-live="assertive"
+        data-delay="3000"
+        aria-atomic="true"
+        style={{ position: "absolute", right: "40%", zIndex: 200 }}
+      >
+        <div className="toast-header" style={{ backgroundColor: "#22b3c1" }}>
+          <img
+            src="assets/images/logo2.png"
+            alt=""
+            className="img-fluid m-r-5"
+            style={{ width: "150px" }}
+          />
+          <strong className="mr-auto"></strong>
+          <small className="text-muted"></small>
+        </div>
+        <div className="toast-body">
+          <strong className="mr-auto">티켓이 장바구니에 담겼습니다.</strong>
+        </div>
+      </div>
 
       <div className="amazing-deals">
         <div className="container">
