2968c20a33a00ad0baac89057e974e052e47319a
HWANG <poka95@naver.com>
2023-09-26 16:59:09 +0900

    .

--- app-server/src/pages/explore/TicketListItem.js
+++ app-server/src/pages/explore/TicketListItem.js
@@ -6,7 +6,7 @@ import TicketDetailItem from "./TicketDetailItem";
 import { push, pop } from "../../store/cartSlice";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
-// axios 기본 url 정의
+
 axios.defaults.baseURL = "http://localhost:4400/api";
 
 const customStyles = {
@@ -15,34 +15,29 @@ const customStyles = {
     left: "50%",
     right: "auto",
     bottom: "auto",
-    // 여기 수정하면 모달창 크기조절
     height: "70vh",
     width: "50vw",
-    //marginRight: "-50%",
     transform: "translate(-50%, -50%)",
-
-    //모달 창 뒤로 배경 너무 뿌옇게 나오는 거 해결 필요!
-    /* 모달 창을 위한 스타일 설정 */
-    backgroundColor: "rgba(255, 255, 255, 0.9)", // 배경을 투명하지 않게 설정
+    backgroundColor: "rgba(255, 255, 255, 0.9)",
     padding: "50px",
-    boxShadow: "0px 0px 10px rgba(0, 0, 0, 3)", // 모달에 그림자 효과 추가
-    //Index: 1000, // 모달을 다른 요소 위에 표시
-
-    // 뒷 배경을 조절하기 위한 스타일 설정
-    backdropFilter: "sepia(90%)", //왜 적용이 안되는 것인가
+    boxShadow: "0px 0px 10px rgba(0, 0, 0, 3)",
+    backdropFilter: "sepia(90%)",
   },
 };
 
 const TicketListItem = function (props) {
   const [modalIsOpen, setIsOpen] = useState(false);
+  const [isVisible, setIsVisible] = useState(true); // 이미지를 보이거나 숨기기 위한 상태 추가
   const appElement = document.getElementById("root");
   const dispatch = useDispatch();
   const myCart = useSelector((state) => state.myCartSlice.myCarts);
+
   if (appElement) {
     Modal.setAppElement(appElement);
   } else {
     console.error("App element not found!");
   }
+
   function openModal() {
     setIsOpen(!modalIsOpen);
   }
@@ -51,11 +46,9 @@ const TicketListItem = function (props) {
     dispatch(
       push({
         ticket: {
-          // 여기 지역추가
           badge: props.festival.addr1 + " " + props.festival.addr2,
           name: props.festival.title,
           quantity: 1,
-          // 여기 가격추가
           price: props.festival.id,
           image: props.festival.first_image2,
           ticket_id: props.festival.id,
@@ -64,6 +57,7 @@ const TicketListItem = function (props) {
       })
     );
   };
+
   const img = props.festival.first_image2;
   const poster =
     props.festival.first_image2 === "" ? (
@@ -93,14 +87,12 @@ const TicketListItem = function (props) {
         ></div>
       </Link>
     );
+
   return (
     <div className="col-lg-6 col-sm-3">
       <div className="item">
         <div className="row">
-          <div className="col-lg-4">
-            {/* 축제 사진 */}
-            {poster}
-          </div>
+          <div className="col-lg-4">{poster}</div>
           <div
             className="col-lg-6 align-self-center"
             onClick={openModal}
@@ -118,38 +110,40 @@ const TicketListItem = function (props) {
                 toCart={toCart}
               />
             </Modal>
-
             <div className="content">
               <span className="info">
                 *D-{props.festival.d_day}
-                끝나요
+                끝나요?
+                {isVisible && (
+                  <img
+                    src="/assets/images/good.jpg"
+                    alt="Good"
+                    style={{ width: "30px" }}
+                  />
+                )}
               </span>
               <h4>{props.festival.title}</h4>
               <div className="row">
                 <div className="col-12">
                   <i className="fa fa-clock"></i>
                   <span className="list">
-                    {"  "}
                     {props.festival.event_start_date} ~{" "}
                     {props.festival.event_end_date}
                   </span>
                 </div>
               </div>
               <p>
-                {props.festival.over_view.substring(0, 38)}{" "}
+                {props.festival.over_view.substring(0, 38)}
                 {props.festival.over_view.length > 38 ? ". . ." : ""}
               </p>
             </div>
           </div>
           <div className="col-lg-2 align-self-center">
-            {/* 홈페이지 연결 */}
             <div className="explore_list_button">
               <Link to={props.festival.home_page} target="_blank">
                 <i className="fa fa-home"></i>
               </Link>
             </div>
-
-            {/* 장바구니 담기 */}
             <div className="explore_list_button" onClick={toCart}>
               <Link onClick={props.alertHandler}>
                 <i className="fa fa-cart-plus"></i>
