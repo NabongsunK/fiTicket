6681e9e57d4b62b1d60ad920cd30663c52535057
jh-makers <dys04009@naver.com>
2023-09-26 14:34:39 +0900

    parking.csv 경로 지정

--- app-server/src/pages/explore/index.js
+++ app-server/src/pages/explore/index.js
@@ -14,6 +14,7 @@ axios.defaults.baseURL = "http://localhost:4400/api";
 
 const getAllMap = async function () {
   const res = await axios.get("/explore/getallmap");
+  console.log(res.data);
   return res.data.data;
 };
 
