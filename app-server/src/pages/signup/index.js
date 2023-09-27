3446f0d8f04ea41298af525e9a1e3cbf9bfba4d4
HWANG <poka95@naver.com>
2023-09-27 13:08:20 +0900

    .

--- app-server/src/pages/signup/index.js
+++ app-server/src/pages/signup/index.js
@@ -12,7 +12,8 @@ function Signup() {
   const [SignupId, setSignupId] = useState();
   const [SignupPn, setSignupPn] = useState();
   const [SignupPw, setSignupPw] = useState();
-  const [SignupRole, setSignupRole] = useState();
+  const [SignupPwConfirm, setSignupPwConfirm] = useState();
+  const [SignupUser, setSignupUser] = useState();
   const [SignupEm, setSignupEm] = useState();
   const [SignupAuth, setSignupAuth] = useState();
   const [SignupName, setSignupName] = useState();
@@ -20,12 +21,70 @@ function Signup() {
   const [isActive, setActive] = useState("false");
   const [isSend, setSend] = useState("false");
 
+  const isValidPhoneNumber = (phoneNumber) => {
+    const regex = /^010-?\d{4}-?\d{4}$/;
+    return regex.test(phoneNumber);
+  };
+
+  const isValidPassword = (password) => {
+    const regex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,}$/;
+    return regex.test(password);
+  };
+
+  const isValidName = (name) => {
+    const regex = /^[가-힣]+$/;
+    return regex.test(name);
+  };
+
+  const isValidEmail = (email) => {
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    return regex.test(email);
+  };
+
+  const isValidId = (id) => {
+    const regex = /^[a-zA-Z0-9]+$/;
+    return regex.test(id);
+  };
+
   const signUp = async function () {
+    // 유효성 검사
+
+    if (!isValidName(SignupName)) {
+      alert("이름은 한글만 가능합니다.");
+      return;
+    }
+
+    if (!isValidPassword(SignupPw)) {
+      alert("비밀번호는 8자리 이상, 영문, 숫자, 특수문자를 포함해야 합니다.");
+      return;
+    }
+
+    if (!isValidEmail(SignupEm)) {
+      alert("유효한 이메일 형식이 아닙니다.");
+      return;
+    }
+
+    if (!isValidId(SignupId)) {
+      alert("ID는 영어와 숫자만 가능합니다.");
+      return;
+    }
+
+    if (SignupPw !== SignupPwConfirm) {
+      alert("비밀번호와 비밀번호 확인이 일치하지 않습니다.");
+      return;
+    }
+
+    if (!SignupPn) {
+      alert("핸드폰 번호를 입력해주세요.");
+      return;
+    }
+
+    // 서버 요청 실행
     const res = await axios.post("/login/signup", {
       login_id: SignupId,
       phone_number: SignupPn,
       password: await hasing(SignupPw),
-      role: SignupRole,
+      role: SignupUser,
       email: SignupEm,
       name: SignupName,
     });
@@ -36,7 +95,15 @@ function Signup() {
     }
     console.log(res);
   };
+
   const getAuth = async function () {
+    if (!isValidPhoneNumber(SignupPn)) {
+      alert(
+        "핸드폰 번호는 010으로 시작하며, 중간,끝에는 4자리 숫자만 가능합니다."
+      );
+      return;
+    }
+
     const res = await axios.post("/auth/getauthnum", {
       login_id: SignupId,
       phone_number: SignupPn,
@@ -62,6 +129,7 @@ function Signup() {
 
     console.log(res);
   };
+
   return (
     // 이쪽 수정부탁
     <section className="signup_page">
@@ -173,19 +241,6 @@ function Signup() {
             </button>
           </div>
 
-          {/* <!-- 비밀번호 --> */}
-          <div className="form-outline mb-4">
-            <input
-              type="password"
-              id="password"
-              className="form-control"
-              onChange={(e) => setSignupPw(e.target.value)}
-            />
-            <label className="form-label" htmlFor="password">
-              비밀번호
-            </label>
-          </div>
-
           {/* <!-- 이름 --> */}
           <div className="form-outline mb-4">
             <input
@@ -199,18 +254,44 @@ function Signup() {
             </label>
           </div>
 
-          {/* <!-- 역할 --> */}
+          {/* <!-- 비밀번호 --> */}
           <div className="form-outline mb-4">
+            <input
+              type="password"
+              id="password"
+              className="form-control"
+              onChange={(e) => setSignupPw(e.target.value)}
+            />
+            <label className="form-label" htmlFor="password">
+              비밀번호
+            </label>
+          </div>
+
+          {/* <!-- 비밀번호 확인 --> */}
+          <div className="form-outline mb-4">
+            <input
+              type="password"
+              id="password_confirm"
+              className="form-control"
+              onChange={(e) => setSignupPwConfirm(e.target.value)}
+            />
+            <label className="form-label" htmlFor="password_confirm">
+              비밀번호 확인
+            </label>
+          </div>
+
+          {/* <!-- 역할 --> */}
+          {/* <div className="form-outline mb-4">
             <input
               type="text"
               id="role"
               className="form-control"
-              onChange={(e) => setSignupRole(e.target.value)}
+              onChange={(e) => setSignupUser(e.target.value)}
             />
             <label className="form-label" htmlFor="role">
               유저/관리자
             </label>
-          </div>
+          </div> */}
 
           {/* <!-- 이메일주소 --> */}
           <div className="form-outline mb-4">
